7fbc2e65995c6d4e7c7d097b490a424044b48a3a
faure <hi@faure.hu>
2015-11-28 16:29:38 +0000

    Request done

--- app/src/main/java/com/example/amosmadalinneculau/googlechartapiexample/MainActivity.java
+++ app/src/main/java/com/example/amosmadalinneculau/googlechartapiexample/MainActivity.java
@@ -1,16 +1,30 @@
 package com.example.amosmadalinneculau.googlechartapiexample;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
 
     private WebView webview;
+    private JSONArray data;
+    private BackgroundTask task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setJavaScriptEnabled(true);
         webview.requestFocusFromTouch();
         webview.loadUrl("file:///android_asset/chartAPI.html");
+        task = new BackgroundTask();
+        task.execute();
     }
 
     @Override
@@ -44,5 +60,50 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class BackgroundTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... Void) {
+            try {
+                data = new JSONArray(readData("http://api.worldbank.org/countries/all/indicators/NY.GDP.MKTP.CD?date=2002:2002&format=json"));
+                System.out.println(data);
+                JSONArray x = (JSONArray) data.get(1);
+                JSONObject y = (JSONObject) x.get(0);
+                Log.v("data", y.get("value").toString());
+//                publishProgress();
+            } catch (IOException e) {
+                System.out.println("Can't read data");
+            } catch (JSONException e) {
+                Log.e("JSON", e.toString());
+            }
+            return null;
+        }
+
+//        @Override
+//        protected void onProgressUpdate(Void... progress) {
+//            setData();
+//        }
+
+        private String readData(String urlName) throws IOException {
+            StringBuffer buffer = new StringBuffer();
+            URL url = new URL(urlName);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.connect();
+            BufferedReader in;
+            in = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+            String inputLine = in.readLine();
+            while (inputLine != null) {
+                buffer.append(inputLine);
+                inputLine = in.readLine();
+            }
+            in.close();
+            connection.disconnect();
+            return buffer.toString();
+        }
     }
 }
