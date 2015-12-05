2315a2266622f2edf54440cd9c62a8423d504c65
Billy Everyteen <nabeel.ahmad_nasir@kcl.ac.uk>
2015-12-05 00:34:06 +0000

    Added circule buttons for countries & modified layout

--- app/src/main/java/com/example/amosmadalinneculau/googlechartapiexample/MainActivity.java
+++ app/src/main/java/com/example/amosmadalinneculau/googlechartapiexample/MainActivity.java
@@ -58,6 +58,10 @@ public class MainActivity extends AppCompatActivity {
 
     private ExplosionField explosionTest;
 
+    at.markushi.ui.CircleButton country1,country2,country3,country4,country5,country6;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -73,17 +77,17 @@ public class MainActivity extends AppCompatActivity {
         vertAnimeTra = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_ver1);
         horAnimeTra = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_hor1);
 
-        /*Intro Explaining Image*/
-        explosionTest = ExplosionField.attach2Window(this);
-        explosion = (ImageView) findViewById(R.id.imageViewExplTest);
-        View.OnClickListener ourOnclickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                explosionTest.explode(v);
-                v.setOnClickListener(null);
-            }
-        };
-        explosion.setOnClickListener(ourOnclickListener);
+        /*Intro Explaining Image (commented temporarily)*/
+//        explosionTest = ExplosionField.attach2Window(this);
+//        explosion = (ImageView) findViewById(R.id.imageViewExplTest);
+//        View.OnClickListener ourOnclickListener = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                explosionTest.explode(v);
+//                v.setOnClickListener(null);
+//            }
+//        };
+//        explosion.setOnClickListener(ourOnclickListener);
 
         initAnim();
         bgSound();
@@ -120,9 +124,6 @@ public class MainActivity extends AppCompatActivity {
         private boolean hasLabelForSelected = false;
         private boolean pointsHaveDifferentColor;
 
-        public PlaceholderFragment() {
-        }
-
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             setHasOptionsMenu(true);
@@ -131,138 +132,12 @@ public class MainActivity extends AppCompatActivity {
             chart = (LineChartView) rootView.findViewById(R.id.chart);
             chart.setOnValueTouchListener(new ValueTouchListener());
 
-            // Generate some randome values.
-            generateValues();
-
-            generateData();
-
-            // Disable viewpirt recalculations, see toggleCubic() method for more info.
+            // Disable viewport recalculations, see toggleCubic() method for more info.
             chart.setViewportCalculationEnabled(false);
 
             resetViewport();
 
             return rootView;
-        }
-
-//        // MENU
-//        @Override
-//        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-//            inflater.inflate(R.menu.line_chart, menu);
-//        }
-//
-//        @Override
-//        public boolean onOptionsItemSelected(MenuItem item) {
-//            int id = item.getItemId();
-//            if (id == R.id.action_reset) {
-//                reset();
-//                generateData();
-//                return true;
-//            }
-//            if (id == R.id.action_add_line) {
-//                addLineToData();
-//                return true;
-//            }
-//            if (id == R.id.action_toggle_lines) {
-//                toggleLines();
-//                return true;
-//            }
-//            if (id == R.id.action_toggle_points) {
-//                togglePoints();
-//                return true;
-//            }
-//            if (id == R.id.action_toggle_cubic) {
-//                toggleCubic();
-//                return true;
-//            }
-//            if (id == R.id.action_toggle_area) {
-//                toggleFilled();
-//                return true;
-//            }
-//            if (id == R.id.action_point_color) {
-//                togglePointColor();
-//                return true;
-//            }
-//            if (id == R.id.action_shape_circles) {
-//                setCircles();
-//                return true;
-//            }
-//            if (id == R.id.action_shape_square) {
-//                setSquares();
-//                return true;
-//            }
-//            if (id == R.id.action_shape_diamond) {
-//                setDiamonds();
-//                return true;
-//            }
-//            if (id == R.id.action_toggle_labels) {
-//                toggleLabels();
-//                return true;
-//            }
-//            if (id == R.id.action_toggle_axes) {
-//                toggleAxes();
-//                return true;
-//            }
-//            if (id == R.id.action_toggle_axes_names) {
-//                toggleAxesNames();
-//                return true;
-//            }
-//            if (id == R.id.action_animate) {
-//                prepareDataAnimation();
-//                chart.startDataAnimation();
-//                return true;
-//            }
-//            if (id == R.id.action_toggle_selection_mode) {
-//                toggleLabelForSelected();
-//
-//                Toast.makeText(getActivity(),
-//                        "Selection mode set to " + chart.isValueSelectionEnabled() + " select any point.",
-//                        Toast.LENGTH_SHORT).show();
-//                return true;
-//            }
-//            if (id == R.id.action_toggle_touch_zoom) {
-//                chart.setZoomEnabled(!chart.isZoomEnabled());
-//                Toast.makeText(getActivity(), "IsZoomEnabled " + chart.isZoomEnabled(), Toast.LENGTH_SHORT).show();
-//                return true;
-//            }
-//            if (id == R.id.action_zoom_both) {
-//                chart.setZoomType(ZoomType.HORIZONTAL_AND_VERTICAL);
-//                return true;
-//            }
-//            if (id == R.id.action_zoom_horizontal) {
-//                chart.setZoomType(ZoomType.HORIZONTAL);
-//                return true;
-//            }
-//            if (id == R.id.action_zoom_vertical) {
-//                chart.setZoomType(ZoomType.VERTICAL);
-//                return true;
-//            }
-//            return super.onOptionsItemSelected(item);
-//        }
-
-        private void generateValues() {
-            for (int i = 0; i < maxNumberOfLines; ++i) {
-                for (int j = 0; j < numberOfPoints; ++j) {
-                    randomNumbersTab[i][j] = (float) Math.random() * 100f;
-                }
-            }
-        }
-
-        private void reset() {
-            numberOfLines = 1;
-
-            hasAxes = true;
-            hasAxesNames = true;
-            hasLines = true;
-            hasPoints = true;
-            shape = ValueShape.CIRCLE;
-            isFilled = false;
-            hasLabels = false;
-            isCubic = false;
-            hasLabelForSelected = false;
-            pointsHaveDifferentColor = false;
-
-            chart.setValueSelectionEnabled(hasLabelForSelected);
-            resetViewport();
         }
 
         private void resetViewport() {
@@ -322,164 +197,6 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-        /**
-         * Adds lines to data, after that data should be set again with
-         * {@link LineChartView#setLineChartData(LineChartData)}. Last 4th line has non-monotonically x values.
-         */
-        private void addLineToData() {
-            if (data.getLines().size() >= maxNumberOfLines) {
-                Toast.makeText(getActivity(), "Samples app uses max 4 lines!", Toast.LENGTH_SHORT).show();
-                return;
-            } else {
-                ++numberOfLines;
-            }
-
-            generateData();
-        }
-
-        private void toggleLines() {
-            hasLines = !hasLines;
-
-            generateData();
-        }
-
-        private void togglePoints() {
-            hasPoints = !hasPoints;
-
-            generateData();
-        }
-
-        private void toggleCubic() {
-            isCubic = !isCubic;
-
-            generateData();
-
-            if (isCubic) {
-                // It is good idea to manually set a little higher max viewport for cubic lines because sometimes line
-                // go above or below max/min. To do that use Viewport.inest() method and pass negative value as dy
-                // parameter or just set top and bottom values manually.
-                // In this example I know that Y values are within (0,100) range so I set viewport height range manually
-                // to (-5, 105).
-                // To make this works during animations you should use Chart.setViewportCalculationEnabled(false) before
-                // modifying viewport.
-                // Remember to set viewport after you call setLineChartData().
-                final Viewport v = new Viewport(chart.getMaximumViewport());
-                v.bottom = -5;
-                v.top = 105;
-                // You have to set max and current viewports separately.
-                chart.setMaximumViewport(v);
-                // I changing current viewport with animation in this case.
-                chart.setCurrentViewportWithAnimation(v);
-            } else {
-                // If not cubic restore viewport to (0,100) range.
-                final Viewport v = new Viewport(chart.getMaximumViewport());
-                v.bottom = 0;
-                v.top = 100;
-
-                // You have to set max and current viewports separately.
-                // In this case, if I want animation I have to set current viewport first and use animation listener.
-                // Max viewport will be set in onAnimationFinished method.
-                chart.setViewportAnimationListener(new ChartAnimationListener() {
-
-                    @Override
-                    public void onAnimationStarted() {
-                        // TODO Auto-generated method stub
-
-                    }
-
-                    @Override
-                    public void onAnimationFinished() {
-                        // Set max viewpirt and remove listener.
-                        chart.setMaximumViewport(v);
-                        chart.setViewportAnimationListener(null);
-
-                    }
-                });
-                // Set current viewpirt with animation;
-                chart.setCurrentViewportWithAnimation(v);
-            }
-
-        }
-
-        private void toggleFilled() {
-            isFilled = !isFilled;
-
-            generateData();
-        }
-
-        private void togglePointColor() {
-            pointsHaveDifferentColor = !pointsHaveDifferentColor;
-
-            generateData();
-        }
-
-        private void setCircles() {
-            shape = ValueShape.CIRCLE;
-
-            generateData();
-        }
-
-        private void setSquares() {
-            shape = ValueShape.SQUARE;
-
-            generateData();
-        }
-
-        private void setDiamonds() {
-            shape = ValueShape.DIAMOND;
-
-            generateData();
-        }
-
-        private void toggleLabels() {
-            hasLabels = !hasLabels;
-
-            if (hasLabels) {
-                hasLabelForSelected = false;
-                chart.setValueSelectionEnabled(hasLabelForSelected);
-            }
-
-            generateData();
-        }
-
-        private void toggleLabelForSelected() {
-            hasLabelForSelected = !hasLabelForSelected;
-
-            chart.setValueSelectionEnabled(hasLabelForSelected);
-
-            if (hasLabelForSelected) {
-                hasLabels = false;
-            }
-
-            generateData();
-        }
-
-        private void toggleAxes() {
-            hasAxes = !hasAxes;
-
-            generateData();
-        }
-
-        private void toggleAxesNames() {
-            hasAxesNames = !hasAxesNames;
-
-            generateData();
-        }
-
-        /**
-         * To animate values you have to change targets values and then call {@link Chart#startDataAnimation()}
-         * method(don't confuse with View.animate()). If you operate on data that was set before you don't have to call
-         * {@link LineChartView#setLineChartData(LineChartData)} again.
-         */
-        private void prepareDataAnimation() {
-            for (Line line : data.getLines()) {
-                for (PointValue value : line.getValues()) {
-                    // Here I modify target only for Y values but it is OK to modify X targets as well.
-                    value.setTarget(value.getX(), (float) Math.random() * 100);
-                }
-            }
-        }
-
         private class ValueTouchListener implements LineChartOnValueSelectListener {
 
             @Override
@@ -488,10 +205,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onValueDeselected() {
-                // TODO Auto-generated method stub
-
-            }
+            public void onValueDeselected() {}
 
         }
     }
@@ -501,20 +215,13 @@ public class MainActivity extends AppCompatActivity {
     /*Intro*/
     public void initAnim() {
 
-        relLayout = (RelativeLayout) findViewById(R.id.relLayout);
-        relLayout.startAnimation(vertAnimeTra);
+        // relLayout = (RelativeLayout) findViewById(R.id.relLayout);
+        //relLayout.startAnimation(vertAnimeTra);
 
         bgMusik = (ToggleButton) findViewById(R.id.toggleMusik);
         bgMusik.startAnimation(horAnimeTra);
 
-        //ExplosionField explosionField =
-        //explosionField.explode(view);
-
-
     }
-
-
-
 
     public void bgSound() {
 
@@ -533,36 +240,42 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     public void choosingCountry() {
 
         /*FloatingActionsMenu 1 & its countries */
-        FloatingActionButton country1 = new FloatingActionButton(getBaseContext());
-        Drawable drawable = ContextCompat.getDrawable(getApplicationContext(), R.drawable.japan_flag);
-        country1.setSize(FloatingActionButton.SIZE_MINI);
-        country1.setBackground(drawable);
-        FloatingActionButton country2 = new FloatingActionButton(getBaseContext());
-        final FloatingActionsMenu menuMultipleActions = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
-        menuMultipleActions.startAnimation(vertAnimeTra);
-        menuMultipleActions.addButton(country1);
-        menuMultipleActions.addButton(country2);
+//        FloatingActionButton country1 = new FloatingActionButton(getBaseContext());
+//        Drawable drawable = ContextCompat.getDrawable(getApplicationContext(), R.drawable.japan_flag);
+//        country1.setSize(FloatingActionButton.SIZE_MINI);
+//        country1.setBackground(drawable);
+//        FloatingActionButton country2 = new FloatingActionButton(getBaseContext());
+//        final FloatingActionsMenu menuMultipleActions = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
+//        menuMultipleActions.startAnimation(vertAnimeTra);
+//        menuMultipleActions.addButton(country1);
+//        menuMultipleActions.addButton(country2);
+//
+//
+//        /*FloatingActionsMenu 2 & its countries */
+//        FloatingActionButton country3 = new FloatingActionButton(getBaseContext());
+//        FloatingActionButton country4 = new FloatingActionButton(getBaseContext());
+//        final FloatingActionsMenu menuMultipleActions2 = (FloatingActionsMenu) findViewById(R.id.multiple_actions2);
+//        menuMultipleActions2.startAnimation(vertAnimeTra);
+//        menuMultipleActions2.addButton(country3);
+//        menuMultipleActions2.addButton(country4);
+//
+//        /*FloatingActionsMenu 3 & its countries */
+//        FloatingActionButton country5 = new FloatingActionButton(getBaseContext());
+//        FloatingActionButton country6 = new FloatingActionButton(getBaseContext());
+//        final FloatingActionsMenu menuMultipleActions3 = (FloatingActionsMenu) findViewById(R.id.multiple_actions3);
+//        menuMultipleActions3.startAnimation(vertAnimeTra);
+//        menuMultipleActions3.addButton(country5);
+//        menuMultipleActions3.addButton(country6);
 
-
-        /*FloatingActionsMenu 2 & its countries */
-        FloatingActionButton country3 = new FloatingActionButton(getBaseContext());
-        FloatingActionButton country4 = new FloatingActionButton(getBaseContext());
-        final FloatingActionsMenu menuMultipleActions2 = (FloatingActionsMenu) findViewById(R.id.multiple_actions2);
-        menuMultipleActions2.startAnimation(vertAnimeTra);
-        menuMultipleActions2.addButton(country3);
-        menuMultipleActions2.addButton(country4);
-
-        /*FloatingActionsMenu 3 & its countries */
-        FloatingActionButton country5 = new FloatingActionButton(getBaseContext());
-        FloatingActionButton country6 = new FloatingActionButton(getBaseContext());
-        final FloatingActionsMenu menuMultipleActions3 = (FloatingActionsMenu) findViewById(R.id.multiple_actions3);
-        menuMultipleActions3.startAnimation(vertAnimeTra);
-        menuMultipleActions3.addButton(country5);
-        menuMultipleActions3.addButton(country6);
+        country1 = (at.markushi.ui.CircleButton) findViewById(R.id.country1);
+        country2 = (at.markushi.ui.CircleButton) findViewById(R.id.country2);
+        country3 = (at.markushi.ui.CircleButton) findViewById(R.id.country3);
+        country4 = (at.markushi.ui.CircleButton) findViewById(R.id.country4);
+        country5 = (at.markushi.ui.CircleButton) findViewById(R.id.country5);
+        country6 = (at.markushi.ui.CircleButton) findViewById(R.id.country6);
 
     }
     //Other Stuff
