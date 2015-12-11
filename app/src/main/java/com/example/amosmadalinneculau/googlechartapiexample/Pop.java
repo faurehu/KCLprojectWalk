c70feef2a402c235d784222a6a4d544c8c94f353
Amos Madalin Neculau <amos.neculau@kcl.ac.uk>
2015-12-11 06:07:49 +0000

    bugs to solve

--- app/src/main/java/com/example/amosmadalinneculau/googlechartapiexample/Pop.java
+++ app/src/main/java/com/example/amosmadalinneculau/googlechartapiexample/Pop.java
@@ -6,18 +6,26 @@ package com.example.amosmadalinneculau.googlechartapiexample;
 
 
         import android.app.Activity;
+        import android.content.Intent;
         import android.os.Bundle;
         import android.util.DisplayMetrics;
+        import android.view.View;
+        import android.widget.Button;
 
 /**
  * Created by Sidd on 09/12/2015.
  */
 public class Pop extends Activity {
 
+    protected Button closeBtn;
+    protected Intent i;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pop_up);
+
+        i = new Intent(this, MainActivity.class);
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -26,6 +34,18 @@ public class Pop extends Activity {
         int height = dm.heightPixels;
 
         getWindow().setLayout((int)(width*0.8),(int)(height*.8));
+
+        closeBtn = (Button) findViewById(R.id.closeBtn);
+
+
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i);
+            }
+        });
+
     }
+
 }
 
